e6356153e4639b6d896863da5c565296e07748d4
Pete <peerapongsm@hotmail.co.th>
2018-10-21 03:19:00 -0700

    add gameplay

--- index.js
+++ index.js
@@ -5,12 +5,8 @@
   function initialize(){
     solution();
     makeGrid();
-    blink(0,1000);
-    //let div = document.createElement("div");
-    //div.setAttribute("id","div");
-    //$("main").appendChild(div);
-    //setTimeout(a,1000);
-    //setTimeout(b,2000);
+    reveal(0,300);
+    setTimeout(play, 4300);
   }
 
   function makeGrid() {
@@ -22,17 +18,19 @@
         div.setAttribute("id", "div");
         $("main").appendChild(div);
         qsa("div")[x].classList.add("cursor");
+        qsa("div")[x].classList.add("wait");
         if (grid[i][j] != null) {
           qsa("div")[x].setAttribute("note",grid[i][j]);
           qsa("div")[x].classList.add("cr");
+          qsa("div")[x].addEventListener("click",blink);
           x++;
           ct++;
        } else {
+         qsa("div")[x].addEventListener("click",blink);
          x++;
        }
       }
       $("main").appendChild(document.createElement("br"));
-
     }
   }
   function solution(){
@@ -42,18 +40,39 @@
             [null,"Gh","Aq",null,null,],
             [null,null,null,null,null]]
   }
-  function blink(i,t) {
+  function reveal(i,t) {
     setTimeout(function(){
         qsa('.cr')[i].classList.add("pink");
     }, t);
     setTimeout(function(){
         qsa('.cr')[i].classList.remove("pink");
-    }, t+1000);
-    if(i < qsa('.cr').length){
-      blink(i+1,t+2000);
+    }, t+300);
+    if(i+1 < qsa('.cr').length){
+      reveal(i+1,t+600);
     }
   }
-
+  function play(){
+    for(let i = 0 ; i < qsa("div").length; i++){
+      qsa("div")[i].classList.remove("wait");
+    }
+  }
+  function blink(){
+    if(this.getAttribute("class") == "cursor cr"){
+      this.classList.add("wait");
+      this.classList.add("lime");
+      setTimeout(function(){
+        qs(".lime").classList.remove("lime");
+        qs(".wait").classList.remove("wait")
+      },1000);
+    } else {
+      this.classList.add("wait");
+      this.classList.add("red")
+      setTimeout(function(){
+        qs(".red").classList.remove("red");
+        qs(".wait").classList.remove("wait")
+      },1000);
+    }
+  }
   function $(id){
     return document.getElementById(id);
   }
