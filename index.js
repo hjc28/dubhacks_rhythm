5356b0de2c22d9cfd1c55134a21ca5ed93931d77
Pete <peerapongsm@hotmail.co.th>
2018-10-21 10:51:29 -0700

    fixing fixing

--- index.js
+++ index.js
@@ -1,53 +1,70 @@
 (function (){
   "use strict";
-  let grid = [];
+
+  const NOTE = ["C3","D3","E3","F3","G3","A3","B3",
+                "C4","D4","E4","F4","G4","A4","B4",
+                "C5","D5","E5","F5","G5","A5","B5"];
+
+  const TWINKLE_LITTLE_STARS = ["C4","C4","G4","G4","A4","A4","G4",
+                                "F4","F4","E4","E4","D4","D4","C4",
+                                "G4","G4","F4","F4","E4","E4","D4",
+                                "G4","G4","F4","F4","E4","E4","D4",
+                                "C4","C4","G4","G4","A4","A4","G4",
+                                "F4","F4","E4","E4","D4","D4","C4"];
+
   window.addEventListener("load", initialize);
   function initialize(){
-    solution();
     makeGrid();
     reveal(0,300);
     setTimeout(play, 4300);
   }
 
   function makeGrid() {
+    let solution = getSolution();
     let x = 0;
-    let ct = 0;
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 5; j++) {
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j <10; j++) {
+        let note_index = Math.floor(Math.random() * 21);
         let div = document.createElement("div");
         div.setAttribute("id", "div");
         $("main").appendChild(div);
         qsa("div")[x].classList.add("cursor");
         qsa("div")[x].classList.add("wait");
-        if (grid[i][j] != null) {
-          qsa("div")[x].setAttribute("note",grid[i][j]);
-          qsa("div")[x].classList.add("cr");
-          qsa("div")[x].addEventListener("click",blink);
-          x++;
-          ct++;
-       } else {
-         qsa("div")[x].addEventListener("click",blink);
-         x++;
-       }
-      }
+        qsa("div")[x].setAttribute("note",NOTE[note_index])
+        if(solution.get(NOTE[note_index]) > 0){
+          qsa("div")[x].setAttribute("cr",0);
+        }
+        qsa("div")[x].addEventListener("click",blink);
+        x++;
+      }/*
+        for(let j = 0; j <10; j++){
+          let list = [];
+          list.push(qsa("div"));
+        }*/
       $("main").appendChild(document.createElement("br"));
     }
   }
-  function solution(){
-    grid = [["Cq","Cq",null,null,null],
-            [null, "Gq", null, null, null ],
-            [null,"Gq","Aq",null,null],
-            [null,"Gh","Aq",null,null,],
-            [null,null,null,null,null]]
+  function getSolution(){
+    let solution = new Map();
+
+    for(let i = 0; i < NOTE.length; i++){
+      solution.set(NOTE[i],0);
+    }
+
+    for(let i  = 0; i < TWINKLE_LITTLE_STARS.length; i++){
+      solution.set(TWINKLE_LITTLE_STARS[i],
+        solution.get(TWINKLE_LITTLE_STARS[i])+1);
+    }
+    return solution;
   }
   function reveal(i,t) {
     setTimeout(function(){
-        qsa('.cr')[i].classList.add("pink");
+        qsa('[cr]')[i].classList.add("pink");
     }, t);
     setTimeout(function(){
-        qsa('.cr')[i].classList.remove("pink");
+        qsa('[cr]')[i].classList.remove("pink");
     }, t+300);
-    if(i+1 < qsa('.cr').length){
+    if(i+1 < qsa('[cr]').length){
       reveal(i+1,t+600);
     }
   }
@@ -57,7 +74,7 @@
     }
   }
   function blink(){
-    if(this.getAttribute("class") == "cursor cr"){
+    if(this.hasAttribute("cr")){
       this.classList.add("wait");
       this.classList.add("lime");
       setTimeout(function(){
